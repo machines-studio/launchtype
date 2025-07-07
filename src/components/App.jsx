589d981794e19eddb85cced41902017d53190d08
Arnaud Juracek <arnaud.juracek@gmail.com>
2025-07-07 10:51:00 +0200

    Allow overriding dpr with ?low=1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -73,7 +73,7 @@ export default class App extends Component {
   patch = {
     sidebarView: +(Constants.SHOW_CABLE_UI),
     textBlendMode: $(Constants.CABLE_BLEND_MODE_NORMAL),
-    maxPixelDensity: window.devicePixelRatio ?? 1,
+    maxPixelDensity: Constants.DPR,
     gradients: $([
       this.store.multA,
       this.store.multB,
